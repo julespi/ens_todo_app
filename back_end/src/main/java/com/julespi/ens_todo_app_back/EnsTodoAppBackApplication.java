d362a260e52fac04adab710479c9d0249c163544
Julian Spinelli <julespi@gmail.com>
2021-10-22 08:47:53 -0300

    init project

---
+++ back_end/src/main/java/com/julespi/ens_todo_app_back/EnsTodoAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.julespi.ens_todo_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsTodoAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsTodoAppBackApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/julespi/ens_todo_app_back/EnsTodoAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julespi.ens_todo_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsTodoAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
